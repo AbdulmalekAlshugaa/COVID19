5506a9b6c4611f5eb5f6f1ce35e3fef6584d809e
Eng_A <Eng_alshugaaye@hotmail.com>
2020-05-17 12:32:06 +0800

    fixing API bugs

---
+++ MyCollegeApp-master/MyCollegeApp-master/app/src/main/java/com/example/mycollegeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.COVID19Tracker.mycollegeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
